3987b1d6b24ef876431b7021e744f02c750ef0b6
Dmitrii Builin <d.builin@mail.ru>
2022-10-26 12:12:45 +0300

    les2 added input

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import './Message.css'
 
 export const Message = ({item}) => {
@@ -20,8 +20,20 @@ export const Message = ({item}) => {
             author: 'author 4',
         }
     ]);
+    const [value, setValue] = useState('');
+    const handleChange = (event) => setValue(event.target.value);
+    const ref = useRef(null);    
+    const form = (event) => {event.preventDefault()};
+    const setInput = () => {ref.current.style.color = 'red'};
+
     return (
         <div>
+            <form onSubmit={form}>
+                <input ref={ref} type="text" value={value} 
+                    onChange={handleChange}/>
+                <button onClick={setInput}>Send</button>
+            </form>
+            
             {MessageList.map(
                 (item) => {
                     return (
