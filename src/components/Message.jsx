df6c9eb2cc898f84029c55961efd7f4764437384
Dmitrii Builin <d.builin@mail.ru>
2022-10-26 21:04:01 +0300

    les2 in progress...

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,7 +1,7 @@
 import { useRef, useState } from 'react'
 import './Message.css'
 
-export const Message = ({item}) => {
+export const Message = () => {
     const [MessageList, setMessageList] = useState([
         {
             text: 'text 1',
@@ -14,37 +14,33 @@ export const Message = ({item}) => {
         {
             text: 'text 3',
             author: 'author 3',
-        },
-        {
-            text: 'text 4',
-            author: 'author 4',
         }
     ]);
     const [value, setValue] = useState('');
     const handleChange = (event) => setValue(event.target.value);
     const ref = useRef(null);    
     const form = (event) => {event.preventDefault()};
-    const setInput = () => {ref.current.style.color = 'red'};
+    // const setInput = () => {ref.setMessageList([...MessageList, { text: {handleChange}, author: 'Bot' }])};
+    const setInput = () => {ref.};
+    // const print = console.log('Hey');
 
     return (
-        <div>
+        <div>           
+            {MessageList.map(
+                (item) => {
+                    return (
+                        <div key={item}>
+                            <h3>{item.author}</h3>
+                            <p>{item.text}</p>
+                        </div>
+                    )
+                }
+            )}
             <form onSubmit={form}>
                 <input ref={ref} type="text" value={value} 
                     onChange={handleChange}/>
                 <button onClick={setInput}>Send</button>
             </form>
-            
-            {MessageList.map(
-                (item) => {
-                    return (
-                        <>
-                            <h3>{item.author}</h3>
-                            <p>{item.text}</p>
-                        </>
-                    )
-                }
-            )}
-            
         </div>
     );
 }
