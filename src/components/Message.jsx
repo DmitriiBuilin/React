c0e3ef4a466037521f5319f29a89e0d8b6c3bf8b
Dmitrii Builin <d.builin@mail.ru>
2022-10-27 00:19:33 +0300

    les2 finished

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react'
+import { useRef, useState, useEffect } from 'react'
 import './Message.css'
 
 export const Message = () => {
@@ -6,40 +6,41 @@ export const Message = () => {
         {
             text: 'text 1',
             author: 'author 1',
-        },
-        {
-            text: 'text 2',
-            author: 'author 2',
-        },
-        {
-            text: 'text 3',
-            author: 'author 3',
         }
     ]);
-    const [value, setValue] = useState('');
+    const [Value, setValue] = useState('');
     const handleChange = (event) => setValue(event.target.value);
     const ref = useRef(null);    
     const form = (event) => {event.preventDefault()};
-    // const setInput = () => {ref.setMessageList([...MessageList, { text: {handleChange}, author: 'Bot' }])};
-    const setInput = () => {ref.};
-    // const print = console.log('Hey');
+    const setInput = () => {
+        setMessageList([...MessageList, 
+            {text: 'Text from the User', author: 'User' }
+        ])
+    };
+
+    useEffect(() => {
+        if (MessageList[MessageList.length - 1]?.author === 'User') { 
+            setTimeout(() => {setMessageList([...MessageList, 
+                { text: 'Something in bot\'s language', author: 'Bot' }]); 
+            }, 1500); } 
+        }, [MessageList]);
 
     return (
         <div>           
             {MessageList.map(
                 (item) => {
                     return (
-                        <div key={item}>
-                            <h3>{item.author}</h3>
-                            <p>{item.text}</p>
+                        <div key={MessageList[item]}>
+                            <h3>Name: {item.author}</h3>
+                            <p>Message: {item.text}</p>
                         </div>
                     )
                 }
             )}
             <form onSubmit={form}>
-                <input ref={ref} type="text" value={value} 
+                <input className='input' ref={ref} type="text" value={Value} 
                     onChange={handleChange}/>
-                <button onClick={setInput}>Send</button>
+                <button className='button' onClick={setInput}>Send</button>
             </form>
         </div>
     );
