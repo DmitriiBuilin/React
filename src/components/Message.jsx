6fac8dd250b51b45e05c4c99c10044dc6510bc47
Dmitrii Builin <d.builin@mail.ru>
2022-11-09 16:40:16 +0300

    Modified styles

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,5 +1,8 @@
 import { useRef, useState, useEffect } from 'react'
 import './Message.css'
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Avatar from '@mui/material/Avatar';
 
 export const Message = () => {
     const [MessageList, setMessageList] = useState([]);
@@ -13,11 +16,42 @@ export const Message = () => {
         ])
     };
 
+    // function stringToColor(string) {
+    //     let hash = 0;
+    //     let i;
+      
+    //     /* eslint-disable no-bitwise */
+    //     for (i = 0; i < string.length; i += 1) {
+    //       hash = string.charCodeAt(i) + ((hash << 5) - hash);
+    //     }
+      
+    //     let color = '#';
+      
+    //     for (i = 0; i < 3; i += 1) {
+    //       const value = (hash >> (i * 8)) & 0xff;
+    //       color += `00${value.toString(16)}`.slice(-2);
+    //     }
+    //     /* eslint-enable no-bitwise */
+      
+    //     return color;
+    //   }
+      
+    //   function stringAvatar(name) {
+    //     return {
+    //       sx: {
+    //         bgcolor: stringToColor(name),
+    //       },
+    //       children: `${name.split(' ')[0][0]}${name.split(' ')[1][0]}`,
+    //     };
+    //   }
+
     useEffect(() => {
-        if (MessageList[MessageList.length - 1]?.author === 'User') { 
+        if (MessageList[MessageList.length - 1]?.author === 'User C') { 
             setTimeout(() => {setMessageList([...MessageList, 
-                { text: "Hallo, Ola, Merhaba, שלןם, Привет, Hello, \
-                Сәлеметсіз бе, 你好, こんにちは, Բարեւ Ձեզ", author: "Bot" }]); 
+                { 
+                    text: "Hallo, Ola, Merhaba, שלןם, Привет, Hello, Сәлеметсіз бе, 你好, こんにちは, Բարեւ Ձեզ", 
+                    author: "Bot B" 
+                }]); 
             }, 1500); } 
         }, [MessageList]);
 
@@ -27,16 +61,24 @@ export const Message = () => {
                 (item) => {
                     return (
                         <div key={MessageList[item]}>
-                            <h3>Name: {item.author}</h3>
+                            <h3><Avatar alt={item.author} src="/src/img/ava.jpg"/> {item.author}</h3>
+                                {/* <Avatar {...stringAvatar('Bob d')} /> */}
                             <p>Message: {item.text}</p>
                         </div>
                     )
                 }
             )}
-            <form onSubmit={form}>
+            <form className='input-form' onSubmit={form}>
                 <input className='input' ref={ref} type="text" value={Value} 
                     onChange={handleChange}/>
-                <button className='button' onClick={setInput}>Send</button>
+                {/* <TextField className='input' ref={ref} id="outlined-basic" 
+                    label="Message" variant="outlined" value={Value} 
+                    onChange={handleChange}
+                /> */}
+                
+                <Button variant="outlined" className='button' 
+                    onClick={setInput}>Send
+                </Button>
             </form>
         </div>
     );
